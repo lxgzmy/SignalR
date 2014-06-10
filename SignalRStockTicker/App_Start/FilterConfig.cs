c873cf2c76fb1ce123f10fe756e7ec97ac342f3b
lxgzmy <lxgzmy@gmail.com>
2014-06-10 17:35:58 +1000

    set up project

---
+++ SignalRStockTicker/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SignalRStockTicker
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SignalRStockTicker/SignalR.Sample/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+[assembly: OwinStartup("Microsoft.AspNet.SignalR.StockTicker.Startup", typeof(Microsoft.AspNet.SignalR.StockTicker.Startup))]
+namespace Microsoft.AspNet.SignalR.StockTicker
+{
+    
+    public static class Startup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application using OWIN startup, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+             app.MapSignalR();
+        }
+    }
+}
